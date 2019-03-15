6c0a8a6a630794c20281d2f76e35df02afa8af06
Taewoong Moon <moondingie5@gmail.com>
2019-03-15 15:15:30 +0900

    Keyboard button module, Divide number, operator function

--- src/App.js
+++ src/App.js
@@ -1,78 +1,40 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import styled, { createGlobalStyle } from 'styled-components';
+import { keyboards } from './keyboards';
 
 
 class App extends Component {
   state = {
     displayNum: 0,
-    keyboards: [
-      {
-        txt: "C"
-      }, 
-      {
-        txt: "+/-"
-      },
-      {
-        txt: "%"
-      },
-      {
-        txt: "/"
-      },
-      {
-        txt: "7"
-      },
-      {
-        txt: "8"
-      },
-      {
-        txt: "9"
-      },
-      {
-        txt: "x"
-      },
-      {
-        txt: "4"
-      },
-      {
-        txt: "5"
-      },
-      {
-        txt: "6"
-      },
-      {
-        txt: "-"
-      },
-      {
-        txt: "1"
-      },
-      {
-        txt: "2"
-      },
-      {
-        txt: "3"
-      },
-      {
-        txt: "+"
-      },
-      {
-        txt: "0"
-      },
-      {
-        txt: "."
-      },
-      {
-        txt: "="
-      }
-    ]
+    keyboards
   }
 
   _clickNumberButton = (number) => {
     console.log('clickNumberButton', number)
   }
 
+  _clickButton = (buttonInfo) => {
+    console.log('_clickButton', buttonInfo)
+    if (buttonInfo.type == "number") {
+      this._handleNumberClick(buttonInfo)
+    } else {
+      this._handleOperator(buttonInfo)
+    }
+  }
+
+  _handleNumberClick = (param) => {
+    console.log('숫자==>', param)
+  }
+
+  _handleOperator = (param) => {
+    console.log('연산자==>', param)
+  }
+
+
   render() {
     const { displayNum, keyboards } = this.state;
+    console.log(1, keyboards)
     const GlobalStyle = createGlobalStyle`
       body {
         padding: 0;
@@ -90,13 +52,21 @@ class App extends Component {
         <GlobalStyle/> 
         {displayNum}
         <Grid container>
-        {keyboards.map((calDom, index) => {
-          return (
-            <Grid item xs={3}>
-              <Button>{calDom.txt}/// {index}</Button>
-            </Grid>
-          )
-        })}
+          {keyboards ? keyboards.map((keyboard, index) => {
+            if (index == 16) {
+              return (
+                <Grid item xs={6} key={index} onClick={() => {this._clickButton(keyboard)}}>
+                  <Button>{keyboard.txt}</Button>
+                </Grid>
+              )
+            } else {
+              return (
+                <Grid item xs={3} key={index} onClick={() => {this._clickButton(keyboard)}} >
+                  <Button>{keyboard.txt}</Button>
+                </Grid>
+              )
+            }
+          }) : ""}
         </Grid>
    
       </React.Fragment>
---
+++ src/keyboards.js
@@ -0,0 +1,79 @@
+export const keyboards = [{
+    txt: "C",
+    type: "operator",
+  },
+  {
+    txt: "+/-",
+    type: "operator",
+  },
+  {
+    txt: "%",
+    type: "operator",
+  },
+  {
+    txt: "/",
+    type: "operator",
+  },
+  {
+    txt: "7",
+    type: "number",
+  },
+  {
+    txt: "8",
+    type: "number",
+  },
+  {
+    txt: "9",
+    type: "number",
+  },
+  {
+    txt: "x",
+    type: "operator",
+  },
+  {
+    txt: "4",
+    type: "number",
+  },
+  {
+    txt: "5",
+    type: "number",
+  },
+  {
+    txt: "6",
+    type: "number",
+  },
+  {
+    txt: "-",
+    type: "operator",
+  },
+  {
+    txt: "1",
+    type: "number",
+  },
+  {
+    txt: "2",
+    type: "number",
+  },
+  {
+    txt: "3",
+    type: "number",
+  },
+  {
+    txt: "+",
+    type: "operator",
+  },
+  {
+    txt: "0",
+    type: "number",
+  },
+  {
+    txt: ".",
+    type: "operator",
+  },
+  {
+    txt: "=",
+    type: "operator",
+  }
+]
+
+// export default keyboards;
